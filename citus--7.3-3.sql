f483405ac1e0d95aab63dbc27dfc08e2979c20da
Brian Cloutier <github@briancloutier.com>
2018-03-27 16:58:32 -0700

    Oops, wrong version of Citus

--- citus--7.3-3.sql
+++ citus--7.3-3.sql
@@ -2667,25 +2667,3 @@ CREATE AGGREGATE pg_catalog.json_cat_agg(json) (
 );
 COMMENT ON AGGREGATE pg_catalog.json_cat_agg(json)
     IS 'concatenate input jsons into a single json';
-/* citus--7.3-3--7.3-4 */
-
-CREATE FUNCTION citus.mitmproxy(text)
-RETURNS text AS $$
-  use strict;
-  use warnings;
-
-  my $command = $_[0];
-  my $filename = '/home/brian/Work/citus/src/test/regress/mitmproxy.fifo';
-
-  my $fh;
-
-  open($fh, '>', $filename) or die 'could not write to mitmproxy';
-  print($fh "$command\n");
-  close($fh);
-
-  open($fh, '<', $filename) or die 'could not read from mitmproxy';
-  my $result = <$fh>;
-  close($fh);
-
-  return $result;
-$$ LANGUAGE plperlu;
